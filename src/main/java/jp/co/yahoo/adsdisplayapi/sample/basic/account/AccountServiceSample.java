d96eebf25db85055811dd2319cecc726d5765b33
ryyoshid <ryyoshid@lycorp.co.jp>
2025-06-30 18:35:17 +0900

    update for v17

--- src/main/java/jp/co/yahoo/adsdisplayapi/sample/basic/account/AccountServiceSample.java
+++ src/main/java/jp/co/yahoo/adsdisplayapi/sample/basic/account/AccountServiceSample.java
@@ -4,14 +4,14 @@
 package jp.co.yahoo.adsdisplayapi.sample.basic.account;
 
 import jp.co.yahoo.adsdisplayapi.sample.util.ApiUtils;
-import jp.co.yahoo.adsdisplayapi.v16.api.AccountServiceApi;
-import jp.co.yahoo.adsdisplayapi.v16.model.Account;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceAutoTaggingEnabled;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceDeliveryStatus;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceOperation;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceSelector;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceStatus;
-import jp.co.yahoo.adsdisplayapi.v16.model.AccountServiceType;
+import jp.co.yahoo.adsdisplayapi.v17.api.AccountServiceApi;
+import jp.co.yahoo.adsdisplayapi.v17.model.Account;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceAutoTaggingEnabled;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceDeliveryStatus;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceOperation;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceSelector;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceStatus;
+import jp.co.yahoo.adsdisplayapi.v17.model.AccountServiceType;
 
 /**
  * example AccountService operation.
